e9a1044e5aed6ec2493576809074c002ae54d8ff
daixuefeng <daixuefeng@exingcai.com>
2016-04-14 16:53:56 +0800

    修改MySQLdb连接参数

--- commlib/storage/mysql/connPool.py
+++ commlib/storage/mysql/connPool.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
-#数据库对象
-#一个简单的数据库链接池
+# -*- coding: utf-8 -*-
+# 数据库对象
+# 一个简单的数据库链接池
 
 import MySQLdb as database
 
+
 class connPool():
     _pools = []
-    def __init__(self, host, db_name, user="", password="", connections=1, time_zone='+08:00', idle_time=5*3600):
+
+    def __init__(self, host, db_name, port=3306, user="", password="",
+                 connections=1, time_zone='+08:00', charset='utf8'):
         connections = int(connections)
         self._pools = []
         self._pool_size = connections
         self._db_index = 0
         init_command = 'SET time_zone="%s"' % time_zone
         for i in xrange(connections):
-            db = database.Connection(host, db_name, user=user, 
-                password=password, max_idle_time=idle_time, time_zone=time_zone)
-            db.execute(init_command)
+            db = database.Connection(host=host, port=port, db=db_name,
+                                     user=user,
+                                     passwd=password, charset=charset,
+                                     autocommit=True,
+                                     init_command=init_command)
+            # db.execute(init_command)
             self._pools.append(db)
 
     @property
